8dd072bf9e2ab9b89a3b8f2198f43f691ca76aae
JesusR25 <18170429@itculiacan.edu.mx>
2025-11-15 08:37:35 -0700

    fix(cookies.py): Arreglamos tokens

--- backend/app/utils/cookies.py
+++ backend/app/utils/cookies.py
@@ -11,6 +11,6 @@ def set_tokens_in_cookies(response: Response, tokens: dict):
     expires_in = tokens.get('expires_in')
     refresh_token = tokens.get('refresh_token')
 
-    response.set_cookie(key='access_token', value=access_token, httponly=True, secure=True, samesite='lax', max_age=expires_in)
+    response.set_cookie(key='access_token', value=access_token, httponly=True, secure=True, samesite='none', max_age=expires_in)
     if refresh_token:
-        response.set_cookie(key='refresh_token', value=refresh_token, httponly=True, secure=True, samesite='lax', max_age=86400)
+        response.set_cookie(key='refresh_token', value=refresh_token, httponly=True, secure=True, samesite='none', max_age=86400)
